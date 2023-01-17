3dbd3f86655a11b001159f5cfafa09bf80f98393
Jeremy Gillen <jez.gillen@gmail.com>
2023-01-17 14:47:13 +0000

    PPO finetuning demo now works

--- soft_optim/ppo_finetune.py
+++ soft_optim/ppo_finetune.py
@@ -1,14 +1,18 @@
 import trlx
 from trlx.data.configs import TRLConfig
 import math
+from pathlib import Path
+
 
 if __name__ == "__main__":
-    def reward_fn(list_of_str):
-        return [s.count('s')/math.sqrt(len(s)) for s in list_of_str]
+    def reward_fn(samples, prompts=None, outputs=None):
+        return [s.count('s') for s in samples]
 
 
+    config_path = Path(__file__).parent / "configs/ppo_gpt2.yml"
+    config = TRLConfig.load_yaml(config_path)
     trlx.train(
         "lvwerra/gpt2-imdb",
         reward_fn=reward_fn,
-        config=TRLConfig.load_yaml("configs/ppo_gpt2.yml"),
+        config=config,
     )
